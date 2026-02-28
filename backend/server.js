b4b550ac43e71e063bab897d5ab10d3c0cb5f1b7
Joseph Li <josel35@uci.edu>
2026-02-27 22:19:58 -0800

    added git ignore

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("REQUEST MADE")
+    res.setHeader('Content-Type', '')
 });
 
 server.listen(3000, 'localhost', () => {
