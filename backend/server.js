d7cb208b268f4cdd2341ff7f2e0a3ec82f047600
Joseph Li <josel35@uci.edu>
2026-02-27 22:45:14 -0800

    Made the server that returns a dud index.html (not ionic)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
+const app = express();
 
-const server = http.createServer((req, res) => {
-    console.log("REQUEST MADE")
-    res.setHeader('Content-Type', '')
+// Allow requests from Ionic frontend
+app.use(cors({
+    origin: 'http://localhost:8100',
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    // use this once the actual server gets running res.sendFile(path.join(__dirname, '../frontend/src/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log("listening")
-})
-
-
+app.listen(3000, () => console.log('Backend running on port 3000'));
