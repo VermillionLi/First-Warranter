3d93596563ac79aa4ab3771e9ea91768b7c75cd9
Clipake <87284709+Clipake@users.noreply.github.com>
2026-02-28 21:27:46 -0800

    appliance-box component

---
+++ frontend/src/app/components/appliance-box/appliance-box.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+
+export interface ApplianceItem { 
+    description: string;
+    price: number,
+}
+@Component({
+  selector: 'app-appliance-box',
+  templateUrl: './appliance-box.component.html',
+  styleUrls: ['./appliance-box.component.scss'],
+})
+export class ApplianceBoxComponent  implements OnInit {
+    @Input() mainLabel: string = "Appliances and Systems";
+    @Input() items: ApplianceItem[] = [];
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
