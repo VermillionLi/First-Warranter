6aaab8626ec4c3597419fd86d7dad6fda02eb07d
Joseph Li <josel35@uci.edu>
2026-02-28 22:34:10 -0800

    50% chancel appliance box works

--- frontend/src/app/components/appliance-box/appliance-box.component.ts
+++ frontend/src/app/components/appliance-box/appliance-box.component.ts
@@ -1,22 +1,70 @@
-import { Component, OnInit, Input} from '@angular/core';
-
+import {Component, OnInit, Input} from '@angular/core';
+import {DataService} from "../../services/data-service";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Injectable} from '@angular/core';
+import {ApplianceStateService} from "../../services/appliance-state-service";
 
 export interface ApplianceItem {
-    description: string;
-    price: number,
+  name: string
+  description: string;
+  price: number,
 }
+
 @Component({
   selector: 'app-appliance-box',
   templateUrl: './appliance-box.component.html',
   styleUrls: ['./appliance-box.component.scss'],
 })
-export class ApplianceBoxComponent  implements OnInit {
-    @Input() mainLabel: string = "Appliances and Systems";
-    @Input() name!: string
-    @Input() items: ApplianceItem[] = [];
-    
-  constructor() { }
 
-  ngOnInit() {}
+
+export class ApplianceBoxComponent implements OnInit {
+  @Input() mainLabel: string = "Appliances and Systems";
+  @Input() name!: string
+  @Input() items: ApplianceItem[] = [];
+
+  constructor(private dataService: DataService, private http: HttpClient, private appliance: ApplianceStateService, private ApplianceItem: ApplianceItem) {
+  }
+
+  ngOnInit() {
+    this.initiateNumbers()
+  }
+
+  initiateNumbers() {
+    const img_dict = this.dataService.getMasterDict()
+    for (const key in img_dict) {
+
+      this.items.push(this.ApplianceItem = {name: key, description: img_dict[key], price: "loading..."})
+    }
+    this.handleNumbers()
+
+  }
+
+  handleNumbers() {
+    const img_dict = this.dataService.getMasterDict()
+    for (const key in img_dict) {
+      const something = this.sendProcesses(key + ", " + img_dict[key])
+      const item = this.items.find(i => i.name === key);
+      Object.assign({name: item, description: img_dict[key], something});
+    }
+  }
+
+
+  sendProcesses(description: string) {
+    type uploadResponse = {
+      body: string
+    }
+    this.http.post<uploadResponse>(`${environment.api_url}/api/calculate`, description).subscribe({
+      next: (res) => {
+        const parsedResponse = JSON.parse(res.body);
+        console.log(`${res.body} files uploaded. Response:`, parsedResponse);
+        //Stephanie update value of whatever to the uhhh thing idk
+        return parsedResponse
+      },
+      error: (err) => {
+        console.error('CALCULATION FAILED?!?!?!?!', err)
+      }
+    });
+  }
 
 }
--- frontend/src/app/components/loading/loading.component.ts
+++ frontend/src/app/components/loading/loading.component.ts
@@ -3,6 +3,7 @@ import {DataService} from "../../services/data-service";
 import {diamond} from "ionicons/icons";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {ApplianceItem} from "../appliance-box/appliance-box.component";
 
 @Component({
   selector: 'app-loading',
@@ -11,43 +12,12 @@ import {HttpClient} from "@angular/common/http";
   providers: [DataService]
 })
 export class LoadingComponent  implements OnInit {
-  constructor(private dataService: DataService, private http: HttpClient) { }
+  constructor(private dataService: DataService, private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.handleNumbers()
-  }
-
-  handleNumbers(){
-    const img_dict = this.dataService.getMasterDict()
-    for (const key in img_dict) {
-        const something = this.sendProcesses(key + ", " + img_dict[key])
-
-    }
 
   }
-
-
-
-  sendProcesses(description:string){
-      type uploadResponse = {
-       body: string
-    }
-    this.http.post<uploadResponse>(`${environment.api_url}/api/calculate`, description).subscribe({
-      next: (res) => {
-        const parsedResponse = JSON.parse(res.body);
-        console.log(`${res.body} files uploaded. Response:`, parsedResponse);
-        //Stephanie update value of whatever to the uhhh thing idk
-        return parsedResponse
-      },
-      error: (err) => {
-        console.error('CALCULATION FAILED?!?!?!?!', err)
-      }
-    });
-  }
-
-
-
-
 }
 
 
